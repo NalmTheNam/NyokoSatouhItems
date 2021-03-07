a0ccf7be712ace9ad48cedb6bbc2af5f88c8cbd3
NalmThe Nalm <nyoko1318@gmail.com>
2021-03-07 13:10:39 +0000

    Back to the update!

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const basicAuth = require("express-basic-auth")
+const rateLimit = require("express-rate-limit");
 const app = express();
 const fs = require("fs");
-const takendown = false;
 const ach = require("./ach.json")
 const banned = []
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 25
+});
 
 // let i = 0
 
@@ -18,10 +22,18 @@ let password = "K3SH-7HHC-2YK3-EFPM-N2US-9M5D-HLTB";
 let items = []
 let deleteditems = [];
 let recentUser = "";
+let admins = {}
 items = JSON.parse(fs.readFileSync("./save/savefle.json"));
 deleteditems = JSON.parse(fs.readFileSync("./save/savefle2.json"));
 recentUser = JSON.parse(fs.readFileSync("./save/savefle3.json"));
-const news = ["There is a total of " + items.length + " items", "Recent name maked an item is " + recentUser]
+admins = JSON.parse(fs.readFileSync("./save/savefle4.json"))
+const basic = basicAuth({
+  users: admins,
+  challenge: true,
+  realm: "Admin Panel",
+  unauthorizedResponse: "Access denied"
+})
+const news = ["There is a total of " + items.length + " items", "Recent name made an item is " + recentUser]
 let randomizeNews = Math.floor(Math.random() * news.length)
 let newsThing = news[randomizeNews]
 setInterval(() => {
@@ -35,44 +47,47 @@ app.enable('case sensitive routing')
 
 app.use(express.static("./static"))
 
+app.use(limiter)
+
 function web(t = "NamItems", a = "", d = false) {
   function disabled() {
-    if (d) return "disabled='true' title='You are already logged in.'"
+    if (d) return "disabled='true' "
+    else return " "
   }
-  return "<!DOCTYPE html><title>" + t + "</title><link href='style.css' rel='stylesheet'><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'><script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'></script><div class='news-tcontainer'><div class='news-ticker'><div class='news-ticker-wrap'><div class='news-ticker-move'>BREAKING NEWS: " + newsThing + "</div></div></div></div><br><div class='items'><h1>Items</h1></div><br>" + items.join("<br>") + "<br><br><button onclick=\"location.href = '/createItem'\">Create an item</button><br><br><button onclick=\"location.href = '/admin'\" " + disabled() + ">Admin Login</button>" + a + "<br><br><button onclick=\"location.href = '/achievements'\" class='card-panel teal lighten-2'>Achievements</button><br><br><nav><button onclick=\"location.href = '/about'\">About</button> <button onclick=\"location.href = '/changelogs'\">Changelogs</button></nav><br>Current Version: v2.15"
+  return "<!DOCTYPE html><title>" + t + "</title><link href='style.css' rel='stylesheet'><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'><script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'></script><script src='script.js' defer></script><div class='news-tcontainer'><div class='news-ticker'><div class='news-ticker-wrap'><div class='news-ticker-move'>BREAKING NEWS: " + newsThing + "</div></div></div></div><br><div class='items'><h1>Items</h1></div><br><button class='waves-effect waves-light btn green' onclick='toggleDarkMode()'>Toggle dark mode</button><br><br>" + items.join("<br>") + "<br><br><button onclick=\"location.href = '/createItem'\" class='waves-effect waves-light btn green'>Create an item</button><br><br><button onclick=\"location.href = '/admin'\" " + disabled() + "class='waves-effect waves-black btn'>Admin Login</button>" + a + "<hr><h4>Misc</h4><hr><button onclick=\"location.href = '/achievements'\" class='card-panel teal lighten-2'>Achievements</button><br><button class='waves-effect waves-light btn black' onclick=\"location.href = '/mods'\">Modifications</button><br><br><nav><button onclick=\"location.href = '/about'\" class='waves-effect waves-light btn-small green'>About</button> <button onclick=\"location.href = '/changelogs'\" class='waves-effect waves-light btn-small green'>Changelogs</button></nav><br>Current Version: v2.22"
 }
 
 app.get("/", (req, res) => {
-  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : takendown ? res.status(429).sendFile(__dirname + "/down.html") : res.send(web())
+  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : res.send(web())
 })
 
 app.get("/about", (req, res) => {
-  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : takendown ? res.status(429).sendFile(__dirname + "/down.html") : res.sendFile(__dirname + "/about.html")
+  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : res.sendFile(__dirname + "/about.html")
 })
 
 app.get("/changelogs", (req, res) => {
-  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : takendown ? res.status(429).sendFile(__dirname + "/down.html") : res.sendFile(__dirname + "/changelogs.html")
+  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : res.sendFile(__dirname + "/changelogs.html")
 })
 
-app.get("/create", (req, res) => {
+app.post("/create", (req, res) => {
   items.push(req.query.user + " made " + req.query.item + " | " + new Date())
   recentUser = req.query.user
 })
 
 app.get("/createItem", (req, res) => {
-  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : takendown ? res.status(429).sendFile(__dirname + "/down.html") : res.sendFile(__dirname + "/create.html")
+  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : res.sendFile(__dirname + "/create.html")
 });
 
 app.get("/achievements", (req, res) => {
-  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : takendown ? res.sendFile(__dirname + "/down.html") : res.send("<!DOCTYPE html><title>NamItems Achievements</title><link href='style.css' rel='stylesheet'><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'><script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'></script><div class='items'><h1>Achievements</h1></div><h3>Completed</h3>" + checkAch(true) + "<h3>Incomplete</h3>" + checkAch() + "<br><button onclick=\"location.href = '/'\">Go back</button>")
+  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : res.send("<!DOCTYPE html><title>NamItems Achievements</title><link href='style.css' rel='stylesheet'><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'><script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'></script><script src='script.js' defer></script><div class='items'><h1>Achievements</h1></div><h3>Completed</h3>" + checkAch(true) + "<h3>Incomplete</h3>" + checkAch() + "<br><button onclick=\"location.href = '/'\" class='waves-effect waves-light btn green'>Go back</button>")
 })
 
 app.get("/admin", (req, res) => {
-  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : takendown ? res.status(429).sendFile(__dirname + "/down.html") : res.sendFile(__dirname + "/login.html")
+  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : res.sendFile(__dirname + "/login.html")
 })
 
-app.get("/didLogin", basicAuth({users:{admin:"NalmTehDack"},challenge:true,realm:"Admin Panel",unauthorizedResponse:"Access denied"}), (req, res) => {
-  banned.includes(req.ip) ? res.status(429).sendFile(__dirname + "/banned.html") : takendown ? res.status(403).sendFile(__dirname + "/down.html") : !req.query.password ? res.status(403).sendFile(__dirname + "/empty.html") : uuid(req.query.password) == password ? res.send(web("NamItems as Admin", "<br><br><button onclick=\"location.replace('/deleteItem?direction=last&password='+location.search.replace('?password=',''))\">Delete last item</button><br><br><button onclick=\"location.replace('/deleteItem?direction=first&password='+location.search.replace('?password=',''))\">Delete first item</button><br><br><button onclick=\"location.replace('/deleteItem?direction=all&password='+location.search.replace('?password=',''))\">Delete all items</button><br><br><button onclick=\"location.href = '/deletedItems'\">Show deleted items</button>", true)) : res.status(403).sendFile(__dirname + "/wrong.html");
+app.get("/didLogin", basic, (req, res) => {
+  banned.includes(req.ip) ? res.status(429).sendFile(__dirname + "/banned.html") : !req.query.password ? res.status(403).sendFile(__dirname + "/empty.html") : uuid(req.query.password) == password ? res.send(web("NamItems as Admin", "<br><br><button onclick=\"location.replace('/deleteItem?direction=last&password='+location.search.replace('?password=',''))\" class='waves-effect waves-light btn red'>Delete last item</button><br><br><button onclick=\"location.replace('/deleteItem?direction=first&password='+location.search.replace('?password=',''))\" class='waves-effect waves-light btn red'>Delete first item</button><br><br><button onclick=\"location.replace('/deleteItem?direction=all&password='+location.search.replace('?password=',''))\" class='waves-effect waves-light btn red'>Delete all items</button><br><br><button onclick=\"location.href = '/deletedItems'\" class='waves-effect waves-light btn green'>Show deleted items</button><br><br><button  class='waves-effect waves-light btn lime'  onclick='location.href = `/createAdminAcc?password=NamTheDuck`'>Create Admin Account</button>", true)) : res.status(403).sendFile(__dirname + "/wrong.html");
 })
 
 app.get("/deleteItem", (req, res) => {
@@ -95,8 +110,26 @@ app.get("/deleteItem", (req, res) => {
   }
 })
 
+app.post("/createAdminAccount", (req, res) => {
+  admins[req.query.name] = req.query.password
+  res.redirect("/didLogin?password=NamTheDuck")
+})
+
+app.get("/createAdminAcc", (req, res) => {
+  if (!req.query.password) return res.status(403).send("Access denied")
+  if (uuid(req.query.password) == password) {
+    res.sendFile(__dirname + "/createadmin.html")
+  } else {
+    res.status(403).send("Access denied");
+  }
+})
+
 app.get("/deletedItems", (req, res) => {
-  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : takendown ? res.status(429).sendFile(__dirname + "/down.html") : res.send("<!DOCTYPE html><title>Deleted items</title><link href='style.css' rel='stylesheet'><link href='style.css' rel='stylesheet'><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'><script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'></script><div class='items'><h1>Deleted items</h1></div><h2>These are non-items. Take care of it.</h2>" + deleteditems.join("<br>") + "<br><br><button onclick=\"location.href = '/'\">Go back</button><br><br><button onclick=\"location.replace('/restoreItem?direction=last&password=NamTheDuck')\">Restore last item</button><br><br><button onclick=\"location.replace('/restoreItem?direction=first&password=NamTheDuck')\">Restore first item</button><br><br><button onclick=\"location.replace('/restoreItem?direction=all&password=NamTheDuck')\">Restore all items</button><div class='danger'><h2>DANGER ZONE</h2><button onclick=\"location.replace('/deletePermamently?direction=last')\">Delete last item permamently</button><br><br><button onclick=\"location.replace('/deletePermamently?direction=first')\">Delete first item permamently</button><br><br><button onclick=\"location.replace('/deletePermamently?direction=all')\">Delete all items permamently</button></div>");
+  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : res.send("<!DOCTYPE html><title>Deleted Items</title><link href='style.css' rel='stylesheet'><link href='style.css' rel='stylesheet'><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'><script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'></script><script src='script.js' defer></script><div class='items'><h1>Deleted Items</h1></div><h2>These are non-items. Take care of it.</h2>" + deleteditems.join("<br>") + "<br><br><button onclick=\"location.href = '/'\" class='waves-effect waves-light btn green'>Go back</button><br><br><button onclick=\"location.replace('/restoreItem?direction=last&password=NamTheDuck')\" class='waves-effect waves-light btn green'>Restore last item</button><br><br><button onclick=\"location.replace('/restoreItem?direction=first&password=NamTheDuck')\" class='waves-effect waves-light btn green'>Restore first item</button><br><br><button onclick=\"location.replace('/restoreItem?direction=all&password=NamTheDuck')\" class='waves-effect waves-light btn green'>Restore all items</button><div class='danger'><h2>DANGER ZONE</h2><button onclick=\"location.replace('/deletePermamently?direction=last')\" class='waves-effect waves-light btn red'>Delete last item permamently</button><br><br><button onclick=\"location.replace('/deletePermamently?direction=first')\" class='waves-effect waves-light btn red'>Delete first item permamently</button><br><br><button onclick=\"location.replace('/deletePermamently?direction=all')\" class='waves-effect waves-light btn red'>Delete all items permamently</button>");
+})
+
+app.get("/mods", (req, res) => {
+  banned.includes(req.ip) ? res.status(403).sendFile(__dirname + "/banned.html") : res.send("<!DOCTYPE html><title>Modifications</title><link href='style.css' rel='stylesheet'><link href='style.css' rel='stylesheet'><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'><script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'></script><script src='script.js' defer></script><div class='items'><h1>Modifications</h1></div><br><a href='//tomitems.tommythecat.repl.co'>https://tomitems.tommythecat.repl.co</a><br><br><button onclick=\"location.href = '/'\" class='waves-effect waves-light btn green'>Go back</button>");
 })
 
 app.get("/deletePermamently", (req, res) => {
@@ -132,8 +165,12 @@ app.get("/restoreItem", (req, res) => {
   }
 })
 
-app.get("/terminal", (req, res) => {
-  res.sendFile(__dirname + "/terminal.html")
+app.get("/randomItem", (req, res) => {
+  res.send("<!DOCTYPE html><title>NamItems</title><pre> " + items[Math.round(Math.random() * items.length - 1)] + " </pre>")
+})
+
+app.get("/redirect", (req, res) => {
+  res.redirect(req.query.to)
 })
 
 app.use((req, res) => {
@@ -153,4 +190,5 @@ setInterval(() => {
   fs.writeFileSync("./save/savefle.json", JSON.stringify(items));
   fs.writeFileSync("./save/savefle2.json", JSON.stringify(deleteditems));
   fs.writeFileSync("./save/savefle3.json", JSON.stringify(recentUser));
+  fs.writeFileSync("./save/savefle4.json", JSON.stringify(admins));
 }, 5000)
--- static/script.js
+++ static/script.js
@@ -1,24 +1,47 @@
-const rick = "location"
+const rick = "href"
+const lS = localStorage
 
 function create() {
   let args = document.getElementById('wip').value
   let argz = document.getElementById('wip1').value
   if (args.startsWith(" ") || argz.startsWith(" ") || !args || !argz) return M.toast({html: "No arguments or blank arguments. (make sure you don't include spaces at the start)", classes: "red"})
-  if (args.includes(rick) || argz.includes(rick)) return M.toast({html: "location is blocked to prevent rickrolls", classes: "red"})
+  if (args.includes(rick) || argz.includes(rick)) return M.toast({html: "href is blocked to prevent rickrolls", classes: "red"})
   let xhr = new XMLHttpRequest()
-  xhr.open("GET", "/create?item=" + args + "&user=" + argz)
+  xhr.open("POST", "/create?item=" + args + "&user=" + argz)
   xhr.send()
   document.getElementById('wip').value = ""
   document.getElementById('wip1').value = ""
-  M.toast({html: 'Created!'})
+  M.toast({html: 'Created!', classes: "green"})
 }
 
 function checkPass() {
   location.href = '/didLogin?password=' + document.getElementById('txt').value
 }
 
-/*
-function checkUsername() {
-  location.href = '/didLogin?username=' + document.getElementById('txt2').value
+function toggleDarkMode() { 
+  if (lS.dark) delete lS.dark
+  else lS.dark = true
+  location.reload()
 }
-*/
+
+function createAdminAcc() {
+  let args = document.getElementById('wip2').value
+  let argz = document.getElementById('wip3').value
+  if (args.startsWith(" ") || argz.startsWith(" ") || !args || !argz) return M.toast({html: "No arguments or blank arguments. (make sure you don't include spaces at the start)", classes: "red"})
+  let xhr = new XMLHttpRequest()
+  xhr.open("POST", "/createAdminAccount?name=" + args + "&password=" + argz)
+  xhr.send()
+  document.getElementById('wip2').value = ""
+  document.getElementById('wip3').value = ""
+  M.toast({html: 'Created!', classes: "green"})
+}
+
+if (lS.dark) {
+  document.body.style.backgroundColor = "#111212"
+  document.body.style.color = "white"
+  document.getElementsByClassName("news-ticker")[0].style.backgroundColor = "white"
+  document.getElementsByClassName("news-ticker")[0].style.color = "black"
+  document.getElementsByClassName("black")[0].style.color = "black"
+  document.getElementsByClassName("black")[0].classList.add("white")
+  document.getElementsByClassName("white")[0].classList.value = "waves-effect waves-black btn black white"
+}
